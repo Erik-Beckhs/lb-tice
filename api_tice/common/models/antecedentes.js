6eccde094a602a7ff91838c8426b8816be4d6ec7
unknown <erikmaquera@gmail.com>
2022-03-20 23:14:18 -0400

    antecedentes remote method

--- api_tice/common/models/antecedentes.js
+++ api_tice/common/models/antecedentes.js
@@ -1,5 +1,27 @@
 'use strict';
 
 module.exports = function(Antecedentes) {
+    Antecedentes.eliminaPorIdConductor = (idConductor, cb) => {
+        var ds = Antecedentes.dataSource;
+        var sql = `
+        delete from antecedentes where id_conductor = ${idConductor};
+        `;
 
+        ds.connector.query(sql, (err, instance) => {
+            if (err) console.error(err);
+            //pubsub.publish('/Pedidoreporte/GET', instance);
+            cb(err, instance);
+        })
+    }
+    
+    Antecedentes.remoteMethod(
+        'eliminaPorIdConductor',
+        {
+            http: { verb: 'delete', path:'/antecedenteConductor'},
+            accepts: { arg: 'idConductor', type: 'string' },
+            returns: { arg: 'data', type: 'any', root: true },
+            description:"Elimina los antecedentes correspondientes a un conductor"
+        }
+    )
 };
+
